27a0b1c581a99ab14a37711b6c1059574feb1214
capitaozila <luis2001sousa@gmail.com>
2023-03-10 16:49:00 -0300

    mais exercicios

--- exemplos/ex001.py
+++ exemplos/ex001.py
@@ -1,1 +1,15 @@
-print('Hello World')
+"""
+\r\n
+aula 01
+
+print('1 + 1')
+print(1 + 1)
+print(12, 34)
+print(12, 34, sep='')
+print(12, 34, sep='-')
+print(12, 34, sep='-', end='\n')
+print(12, 34, sep='-', end='\r')
+print(12, 34, sep='-', end='\n')
+"""
+
+#aula 22
---
+++ exemplos/ex002.py
@@ -0,0 +1,19 @@
+nome = "João"
+sobrenome = "Silva"
+idade = 20
+ano_nascimento = 2023 - idade
+altura = 1.80
+peso = 80.5
+maior_idade = idade >= 18
+
+print("Nome: ", nome, sobrenome)
+print("Idade: ", idade)
+print("Data de nascimento: ", ano_nascimento)
+print("É maior de idade: ", maior_idade)
+print("Altura: ", altura, "m", sep="")
+print("Peso: ", peso, sep="")
+
+# if (maior_idade == True):
+#     print("É maior de idade")
+# else:
+#     print("É menor de idade")
