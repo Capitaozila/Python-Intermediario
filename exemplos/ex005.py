8ac01bdcd134f590a7e7f2362f248e31a193aa9b
capitaozila <luis2001sousa@gmail.com>
2023-03-11 19:20:58 -0300

    adicionando mais exemplos

---
+++ exemplos/ex005.py
@@ -0,0 +1,6 @@
+variavel = 'ABC'
+
+# print(f'{variavel}')
+print(f'{variavel:.^20}')
+print(f'{variavel:.^8}{variavel}{variavel:.^9}')
+print(f'{variavel:.^20}')
---
+++ exemplos/interpolação_strings.py
@@ -0,0 +1,10 @@
+carro_fabricante = "Fiat"
+carro_ano = '2001'
+carro_nome = 'Uno'
+d = 10
+
+carro = 'É um carro %s, %s, %s' % (carro_fabricante, carro_nome, carro_ano)
+
+print(carro)
+print('O hexadecimal de %d é %x' % (9, 9))
+print('O hexadecimal de %d é %04X' % (15, 15))
