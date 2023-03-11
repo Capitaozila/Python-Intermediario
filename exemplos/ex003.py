49f59127c11808c8cfc1da035a0e65cdac8f52a0
capitaozila <luis2001sousa@gmail.com>
2023-03-11 11:42:36 -0300

    adicionando mais exercicios

---
+++ exemplos/ex003.py
@@ -0,0 +1,16 @@
+"""
+aula 11
+ 1. (n + n)
+ 2. **
+ 3. * / // %
+ 4. + -
+conta_1 = (1 + int(0.5 + 0.5)) ** (5 + 5)
+print(conta_1)
+"""
+
+nome = "Luis Henrique"
+altura = 1.70
+peso = 65.0
+imc = peso / (altura ** 2)
+
+print(f"{nome} tem {altura} metros de altura \n pesa {peso} quilos \r Seu IMC é: {imc:.2f}")
