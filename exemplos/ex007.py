46754beae32b6795d82cb8127fed9745eab62487
capitaozila <luis2001sousa@gmail.com>
2023-03-12 22:10:09 -0300

    .

--- exemplos/ex007.py
+++ exemplos/ex007.py
@@ -7,7 +7,7 @@ if (nome and idade):
     print(f'Sua idade é de {idade} anos')
     print(f'Seu nome invertido é "{nome[::-1]}"')
     if (" " in nome):
-        print('Seu nome contém espaços')
+        print('Seu nome contém espaços.')
     else:
         print('Seu nome não contém espaços')
     print(f'Seu nome tem {len(nome)} letras')
