48022fb3ea929f1ff22a7c6ebab3a0944c2067e8
capitaozila <luis2001sousa@gmail.com>
2023-03-24 22:21:27 -0300

    corrigido o bug que o segundo digito do cpf

--- exemplos/validador_cpf.py
+++ exemplos/validador_cpf.py
@@ -43,17 +43,17 @@ Ex.:  027.908.343-26 (02790834326)
 """
 
 
-cpf_enviado_usuario = '74682489070'
+cpf_enviado_usuario_1 = '74682489070'
 cpf_enviado_usuario_2 = '02790834326'
-cpf_enviado_usuario_3 = '134.796.490-84'
+cpf_enviado_usuario_3 = '13479649084'
 
-nove_digitos = cpf_enviado_usuario_2[:9]
+nove_digitos = cpf_enviado_usuario_3[:9]
 
 contador_regressivo_1 = 10
 
 resultado_digito_1 = 0
-for digitos in nove_digitos:
-    resultado_digito_1 += int(digitos) * contador_regressivo_1
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
     contador_regressivo_1 -= 1
 
 digito_1 = (resultado_digito_1 * 10) % 11
@@ -63,17 +63,15 @@ dez_digitos = nove_digitos + str(digito_1)
 contador_regressivo_2 = 11
 
 resultado_digito_2 = 0
-
 for digito in dez_digitos:
-    resultado_digito_2 += int(digitos) * contador_regressivo_2
+    resultado_digito_2 += int(digito) * contador_regressivo_2
     contador_regressivo_2 -= 1
-
 digito_2 = (resultado_digito_2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
 
 cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
 
-if cpf_enviado_usuario_2 == cpf_gerado_pelo_calculo:
-    print(f'Seu cpf "{cpf_enviado_usuario_2}" é válido')
+if cpf_enviado_usuario_3 == cpf_gerado_pelo_calculo:
+    print(f'Seu cpf "{cpf_enviado_usuario_3}" é válido')
 else:
-    print(f'Seu cpf "{cpf_enviado_usuario_2}" não é válido')
+    print(f'Seu cpf "{cpf_enviado_usuario_3}" não é válido')
