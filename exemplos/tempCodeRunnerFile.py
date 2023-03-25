e68b9aced84257b966a56de4ff278cdeacfaed2c
capitaozila <luis2001sousa@gmail.com>
2023-03-24 22:08:14 -0300

    gerador

--- exemplos/tempCodeRunnerFile.py
+++ exemplos/tempCodeRunnerFile.py
@@ -1,2 +1,34 @@
-for x in range(0, 3):
-#     print('-----------------')
+
+cpf_enviado_usuario = '74682489070'
+cpf_enviado_usuario_2 = '02790834326'
+
+nove_digitos = cpf_enviado_usuario[:9]
+
+contador_regressivo_1 = 10
+
+resultado_digito_1 = 0
+for digitos in nove_digitos:
+    resultado_digito_1 += int(digitos) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+
+digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0
+
+for digito in dez_digitos:
+    resultado_digito_2 += int(digitos) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+    print(f'Seu cpf "{cpf_enviado_usuario}" é válido')
+else:
+    print(f'Seu cpf "{cpf_enviado_usuario}" não é válido')
