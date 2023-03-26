00aade74fa3533a6c1d614fc95fee338c6ef2c96
capitaozila <luis2001sousa@gmail.com>
2023-03-26 18:35:51 -0300

    aula 109

--- exemplos/provas/validador_cpf.py
+++ exemplos/provas/validador_cpf.py
@@ -43,37 +43,37 @@ Ex.:  027.908.343-26 (02790834326)
 """
 
 
-import randomimport sys
+import random, sys
 for _ in range (100):
 
-    nove_digitos = ''
+   nove_digitos = ''
 
-    for i in range(9):
-        nove_digitos += str(random.randint(0, 9))
+   for i in range(9):
+      nove_digitos += str(random.randint(0, 9))
 
-    """Calcula os dígitos verificadores do CPF."""
-    contador_regressivo_1 = 10
+   """Calcula os dígitos verificadores do CPF."""
+   contador_regressivo_1 = 10
 
-    resultado_digito_1 = 0
-    for digito in nove_digitos:
-        resultado_digito_1 += int(digito) * contador_regressivo_1
-        contador_regressivo_1 -= 1
-    """Valida o CPF."""
-    digito_1 = (resultado_digito_1 * 10) % 11
-    digito_1 = digito_1 if digito_1 <= 9 else 0
+   resultado_digito_1 = 0
+   for digito in nove_digitos:
+      resultado_digito_1 += int(digito) * contador_regressivo_1
+      contador_regressivo_1 -= 1
+   """Valida o CPF."""
+   digito_1 = (resultado_digito_1 * 10) % 11
+   digito_1 = digito_1 if digito_1 <= 9 else 0
 
-    dez_digitos = nove_digitos + str(digito_1)
-    contador_regressivo_2 = 11
+   dez_digitos = nove_digitos + str(digito_1)
+   contador_regressivo_2 = 11
 
-    resultado_digito_2 = 0
-    for digito in dez_digitos:
-        resultado_digito_2 += int(digito) * contador_regressivo_2
-        contador_regressivo_2 -= 1
-    digito_2 = (resultado_digito_2 * 10) % 11
-    digito_2 = digito_2 if digito_2 <= 9 else 0
+   resultado_digito_2 = 0
+   for digito in dez_digitos:
+      resultado_digito_2 += int(digito) * contador_regressivo_2
+      contador_regressivo_2 -= 1
+   digito_2 = (resultado_digito_2 * 10) % 11
+   digito_2 = digito_2 if digito_2 <= 9 else 0
 
-    cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+   cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
 
-    print(cpf_gerado_pelo_calculo)
+   print(cpf_gerado_pelo_calculo, "CPF VÁLIDO")
 
     # código refatorado
