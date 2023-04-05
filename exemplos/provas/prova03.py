e1170f0d1874eeaa2235474bd7c34c902dbc258d
capitaozila <luis2001sousa@gmail.com>
2023-04-04 22:25:00 -0300

    refatorando codigo

--- exemplos/provas/prova03.py
+++ exemplos/provas/prova03.py
@@ -9,21 +9,15 @@ while True:
     numero_tentativas += 1
 
     if len(letra_digitada) > 1:
-        print('DIgite apenas uma letra')
+        print('Digite apenas uma letra.')
         continue
 
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
 
-    palavra_formada = ''
+    palavra_formada = ''.join([letra_secreta if letra_secreta in letras_acertadas else '*' for letra_secreta in palavra_secreta])
 
-    for letra_secreta in palavra_secreta:
-        if letra_secreta in letras_acertadas:
-            palavra_formada += letra_secreta
-        else:
-            palavra_formada += '*'
-
-    print('Palavra formada: ', palavra_formada)
+    print('Palavra formada:', palavra_formada)
 
     if palavra_formada == palavra_secreta:
         os.system('cls' if os.name == 'nt' else 'clear')
