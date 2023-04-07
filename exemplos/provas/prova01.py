8c17a0bcd1df0417c559458c5d0d0a018f928036
capitaozila <luis2001sousa@gmail.com>
2023-04-06 22:14:35 -0300

    refatorado

--- exemplos/provas/prova01.py
+++ exemplos/provas/prova01.py
@@ -1,10 +1,9 @@
-
-# numero = input('Digite um número inteiro: ')
-# try:
-#     numero_int = int(numero)
-#     if numero_int % 2 == 0:
-#         print(f'{numero} é par')
-#     else:
-#         print(f'{numero} é impar')
-# except:
-#     print(f'"{numero}" não é um numero inteiro')
+numero = input('Digite um número inteiro: ')
+try:
+    numero_int = int(numero)
+    if numero_int % 2 == 0:
+        print(f'{numero_int} é par')
+    else:
+        print(f'{numero_int} é ímpar')
+except ValueError:
+    print(f'"{numero}" não é um número inteiro')
