20c3442a9d04d74674d70390391c5c2e2dfb90fe
Capitaozila <luis2001sousa@gmail.com>
2024-01-21 11:50:43 -0300

    Exercício com classes e relações resolvido

--- exemplos/secao_5/exercicios/ex01.py
+++ exemplos/secao_5/exercicios/ex01.py
@@ -7,3 +7,67 @@
 # 5 - Faça a ligação entre Carro e Fabricante
 # Obs.: Um fabricante pode fabricar vários carros
 # Exiba o nome do carro, motor e fabricantes na tela
+
+
+class Carro():
+    def __init__(self, nome):
+        self.nome = nome
+        self._motor = None
+        self._fabricante = None
+
+    @property
+    def motor(self):
+        return self._motor
+
+    @motor.setter
+    def motor(self, valor):
+        self._motor = valor
+
+    @property
+    def fabricante(self):
+        return self._fabricante
+
+    @fabricante.setter
+    def fabricante(self, valor):
+        self._fabricante = valor
+
+    def inserir_motor(self):
+        print('Carro tem MOTOR')
+
+
+class Motor():
+    def __init__(self, nome):
+        self.nome = nome
+
+
+class Fabricante():
+    def __init__(self, nome):
+        self.nome = nome
+
+
+fusca = Carro('Fusca')
+volkswagen = Fabricante('Volkswagen')
+motor_1_0 = Motor('1.0')
+fusca.fabricante = volkswagen
+fusca.motor = motor_1_0
+print(fusca.nome, fusca.fabricante.nome, fusca.motor.nome)
+
+fiat_uno = Carro('Uno')
+fiat = Fabricante('Fiat')
+fiat_uno.fabricante = fiat
+fiat_uno.motor = motor_1_0
+print(fiat_uno.nome, fiat_uno.fabricante.nome, fiat_uno.motor.nome)
+
+# terceiro carro
+palio = Carro('Palio')
+palio.fabricante = fiat
+palio.motor = motor_1_0
+print(palio.nome, palio.fabricante.nome, palio.motor.nome)
+
+# quarto carro
+focus = Carro('Focus Titanium')
+Ford = Fabricante('Ford')
+motor_2_0 = Motor('2.0')
+focus.fabricante = Ford
+focus.motor = motor_2_0
+print(focus.nome, focus.fabricante.nome, focus.motor.nome)
