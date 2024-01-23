746842376e1cfc427dfeb3fb064a7d047d21bdb3
Capitaozila <luis2001sousa@gmail.com>
2024-01-22 21:33:53 -0300

    Log, LogFileMixin, LogPrintMixin e a união de tudo até aqui

--- exemplos/secao_5/ex034/log.py
+++ exemplos/secao_5/ex034/log.py
@@ -1,13 +1,24 @@
 class Log:
-    def log(self, mensagem):
+    def _log(self, mensagem):
         raise NotImplementedError('Implemente o método log')
+
+    def log_error(self, mensagem):
+        return self._log(f'Error: {mensagem}')
     
+    def log_sucess(self, mensagem):
+        return self._log(f'Sucess: {mensagem}')
 
 class LogFileMixin(Log):
-    def log(self, mensagem):
+    def _log(self, mensagem):
         print(mensagem)
-    
-    
+
+
+class LogPrintMixin(Log):
+    def _log(self, mensagem):
+        print(f'{mensagem} ({self.__class__.__name__})')
+
+
 if __name__ == '__main__':
-    l = LogFileMixin()
-    l.log('qualquer coisa')
+    l = LogPrintMixin()
+    l.log_error('qualquer coisa')
+    l.log_sucess('deu certo!')
