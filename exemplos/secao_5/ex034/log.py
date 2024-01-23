e46885633e56e1071e9ebe3684f2cd7d77d6d626
Capitaozila <luis2001sousa@gmail.com>
2024-01-22 21:46:12 -0300

    LogFileMixin e a união de tudo até aqui

--- exemplos/secao_5/ex034/log.py
+++ exemplos/secao_5/ex034/log.py
@@ -1,16 +1,26 @@
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+
 class Log:
     def _log(self, mensagem):
         raise NotImplementedError('Implemente o método log')
 
     def log_error(self, mensagem):
         return self._log(f'Error: {mensagem}')
-    
+
     def log_sucess(self, mensagem):
         return self._log(f'Sucess: {mensagem}')
 
+
 class LogFileMixin(Log):
     def _log(self, mensagem):
-        print(mensagem)
+        mensagem_formatada = f'{mensagem} ({self.__class__.__name__})'
+        print('SALVANDO NO LOG...', mensagem_formatada)
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(mensagem_formatada)
+            arquivo.write('\n')
 
 
 class LogPrintMixin(Log):
@@ -19,6 +29,9 @@ class LogPrintMixin(Log):
 
 
 if __name__ == '__main__':
-    l = LogPrintMixin()
-    l.log_error('qualquer coisa')
-    l.log_sucess('deu certo!')
+    log_print = LogPrintMixin()
+    log_print.log_error('qualquer coisa')
+    log_print.log_sucess('deu certo!')
+    log_file = LogFileMixin()
+    log_file.log_error('qualquer coisa')
+    log_file.log_sucess('deu certo!')
--- exemplos/secao_5/ex034/main.py
+++ exemplos/secao_5/ex034/main.py
@@ -1,1 +1,8 @@
-print('oi')
+from log import LogPrintMixin, LogFileMixin
+
+log_print = LogPrintMixin()
+log_print.log_error('qualquer coisa')
+log_print.log_sucess('deu certo!')
+log_file = LogFileMixin()
+log_file.log_error('qualquer coisa')
+log_file.log_sucess('deu certo!')
