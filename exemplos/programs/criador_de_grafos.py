2471b249feeeb576c144bea9b1f60b18c994d5cf
Capitaozila <luis2001sousa@gmail.com>
2024-04-17 14:29:53 -0300

    adicionando algoritmo criador de grafo simples

---
+++ exemplos/programs/criador_de_grafos.py
@@ -0,0 +1,31 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Cria um grafo vazio
+G = nx.Graph()
+
+# Adiciona nós ao grafo
+G.add_node(0)
+G.add_node(1)
+G.add_node(2)
+G.add_node(3)
+G.add_node(4)
+
+
+# Adiciona arestas ao grafo
+G.add_edge(0, 1)
+G.add_edge(0, 2)
+G.add_edge(1, 0)
+G.add_edge(1, 3)
+G.add_edge(1, 2)
+G.add_edge(2, 0)
+G.add_edge(2, 1)
+G.add_edge(2, 3)
+G.add_edge(2, 4)
+G.add_edge(3, 1)
+G.add_edge(3, 2)
+G.add_edge(4, 2)
+
+# Desenha o grafo
+nx.draw(G, with_labels=True)
+plt.show()
