b3ad03d5367b1ec3ef37938cbd13673722765547
Capitaozila <luis2001sousa@gmail.com>
2024-06-01 17:50:25 -0300

    melhorando o codigo de busca em pronfudidade

--- exemplos/programs/grafos/busca_em_profundidade.py
+++ exemplos/programs/grafos/busca_em_profundidade.py
@@ -1,33 +1,23 @@
 def dfs(grafo, raiz):
-    # Cria um conjunto para armazenar os vértices visitados
-    visitado = {raiz}
-    
-    # Cria uma pilha e adiciona o vértice raiz
-    pilha = [raiz]
+    visitado = set()  # Cria um conjunto vazio para armazenar os vértices visitados
+    pilha = [raiz]  # Cria uma pilha e adiciona o vértice raiz
 
     while pilha:
-        # Remove o próximo vértice da pilha
-        vertice = pilha.pop()
-        
-        # Imprime o vértice atual
-        print(f"Visitando o vértice: {vertice}")
+        vertice = pilha.pop()  # Remove o último vértice da pilha
+        if vertice not in visitado:
+            visitado.add(vertice)  # Marca o vértice como visitado
+            print(f"Visitando o vértice: {vertice}")
 
-        # Percorre todos os vizinhos do vértice atual
-        for vizinho in grafo[vertice]:
-            # Se o vizinho ainda não foi visitado...
-            if vizinho not in visitado:
-                # Marca o vizinho como visitado
-                visitado.add(vizinho)
-                
-                # Adiciona o vizinho à pilha
-                pilha.append(vizinho)
-                print(f"Adicionando {vizinho} à pilha")
+            for vizinho in reversed(grafo[vertice]):
+                if vizinho not in visitado:
+                    pilha.append(vizinho)  # Adiciona o vizinho à pilha
+                    print(f"Adicionando {vizinho} à pilha")
 
 # Exemplo de uso:
 grafo = {
     '1': ['2', '4'],
     '2': ['3', '1'],
-    '3': ['2', '6'],
+    '3': ['2', '4','6'],
     '4': ['5', '1'],
     '5': ['4'],
     '6': ['3']
